7e97968521b3d4b5f26deca3b1bc95bbee87ccc9
Tomas Hubelbauer <tomas@hubelbauer.net>
2023-08-31 13:56:50 +0200

    Set up Playwright for E2E tests
    I configured it such that it should work in GitHub Actions CI as well.

---
+++ playwright.config.ts
@@ -0,0 +1,31 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: false,
+  forbidOnly: !!process.env.CI,
+  retries: 0,
+  workers: 1,
+  reporter: [
+    [process.env.CI ? 'github' : 'list'],
+    ['html', { open: 'never' }]
+  ],
+  use: {
+    baseURL: 'http://127.0.0.1:3000',
+    trace: 'on',
+    headless: false,
+    screenshot: 'on',
+    video: 'on',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    }
+  ],
+  webServer: {
+    command: 'npm start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
+});
